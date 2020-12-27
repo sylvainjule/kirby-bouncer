4ca2e3273af8a1fe284010f3a722655f351b3ed4
Sylvain Julé <sylvain.jule@free.fr>
2020-12-27 15:03:29 +0100

    Check if system is ready before setting up hooks. Closes #3

--- src/index.js
+++ src/index.js
@@ -5,70 +5,80 @@ panel.plugin('sylvainjule/bouncer', {
         bouncernav: BouncerNav
     },
     created(Vue) {
-        watchUser(Vue);
+        Vue.$store
+            .dispatch("system/load")
+            .then(system => {
+                // prevents an infinite loop on login page when there is no account
+                if(!system.isReady) return true;
 
-        Vue.$router.onReady(function() {
-            getCurrentUser(true, function() {
-                checkRedirect(Vue.$router.currentRoute, undefined);
-            });
-        });
+                watchUser(Vue);
 
-        Vue.$router.beforeResolve((to, from, next) => {
-            if(from.path == '/login') Vue.$data.user = undefined;
-
-            if(!Vue.$data.user) {
-                getCurrentUser(false, function() {
-                    checkRedirect(to, next);
+                Vue.$router.onReady(function() {
+                    getCurrentUser(true, function() {
+                        checkRedirect(Vue.$router.currentRoute, undefined);
+                    });
                 });
-            }
-            else {
-                checkRedirect(to, next);
-            }
-        });
+
+                Vue.$router.beforeResolve((to, from, next) => {
+                    if(from.path == '/login') Vue.$data.user = undefined;
+
+                    if(!Vue.$data.user) {
+                        getCurrentUser(false, function() {
+                            checkRedirect(to, next);
+                        });
+                    }
+                    else {
+                        checkRedirect(to, next);
+                    }
+                });
 
 
-        function checkRedirect(to, next, firstLoad = false) {
-            var user = Vue.$data.user;
+                function checkRedirect(to, next, firstLoad = false) {
+                    var user = Vue.$data.user;
 
-            if(user && Array.isArray(user.restriction) && user.restriction.length) {
-                if(!isAllowed(to, user)) {
-                    if(next) { next(user.restriction[0].path); }
-                    else     { Vue.$router.push(user.restriction[0].path); }
-                }
-                else {
-                    if(next) {
-                        if(to.path == '/logout' || to.path == '/login') Vue.$data.user = undefined;
-                        next();
+                    if(user && Array.isArray(user.restriction) && user.restriction.length) {
+                        if(!isAllowed(to, user)) {
+                            if(next) { next(user.restriction[0].path); }
+                            else     { Vue.$router.push(user.restriction[0].path); }
+                        }
+                        else {
+                            if(next) {
+                                if(to.path == '/logout' || to.path == '/login') Vue.$data.user = undefined;
+                                next();
+                            }
+                        }
+                    }
+                    else {
+                        if(next) { next(); }
                     }
                 }
-            }
-            else {
-                if(next) { next(); }
-            }
-        }
-        function getCurrentUser(safety = false, callback = false) {
-            if(safety && (Vue.$router.currentRoute.path == '/login' || Vue.$router.currentRoute.path == '/')) return false;
+                function getCurrentUser(safety = false, callback = false) {
+                    if(safety && (Vue.$router.currentRoute.path == '/login' || Vue.$router.currentRoute.path == '/')) return false;
 
-            Vue.$api
-                .get('current-user')
-                .then(user => {
-                    Vue.$data.user = user;
-                    if(callback) callback();
-                });
-        }
-        function isAllowed(to, user) {
-            let allowed = to.name == 'Account' || to.path == '/account' || to.path == '/logout' || to.path == '/login';
+                    Vue.$api
+                        .get('current-user')
+                        .then(user => {
+                            Vue.$data.user = user;
+                            if(callback) callback();
+                        })
+                        .catch(error => {
+                            if(callback) callback();
+                        });
+                }
+                function isAllowed(to, user) {
+                    let allowed = to.name == 'Account' || to.path == '/account' || to.path == '/logout' || to.path == '/login';
 
-            if(!allowed && Array.isArray(user.restriction)) {
-                user.restriction.forEach(page => {
-                    if(to.path.slice(0, page.path.length) == page.path) {
-                        allowed = true;
+                    if(!allowed && Array.isArray(user.restriction)) {
+                        user.restriction.forEach(page => {
+                            if(to.path.slice(0, page.path.length) == page.path) {
+                                allowed = true;
+                            }
+                        })
                     }
-                })
-            }
 
-            return allowed;
-        }
+                    return allowed;
+                }
+            })
     },
 });
 
