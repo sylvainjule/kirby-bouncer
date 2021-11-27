05dcbdebbf5937939d75f15773c77b26a974323d
Sylvain Julé <sylvain.jule@free.fr>
2021-11-27 16:17:21 +0100

    Remove unnecessary code

--- src/index.js
+++ src/index.js
@@ -4,34 +4,4 @@ panel.plugin('sylvainjule/bouncer', {
     sections: {
         bouncernav: BouncerNav
     },
-    created(Vue) {
-        console.log('ok')
-    },
 });
-
-function watchUser(app) {
-    app.$router.afterEach((to, from) => {
-        updateUser(app)
-    });
-    app.$store.watch(
-        state => {
-            if (state.user.current) { return state.user.current; }
-        },
-        (newUser, oldUser) => {
-            updateUser(app)
-        }
-    )
-}
-function updateUser(app, user) {
-    var user = app.$data.user
-
-    app.$el.classList.remove('bouncer-padding-top');
-
-    if(user && user.nav && Array.isArray(user.restriction) && user.restriction.length > 1) {
-        var paths = user.restriction.map(el => { return el.path })
-
-        if(paths.includes(app.$router.currentRoute.path)) {
-            app.$el.classList.add('bouncer-padding-top');
-        }
-    }
-}
